6b888b6d57f339eadaf6d14df10b506894b3e156
Noah Kantrowitz <noah@coderanger.net>
2022-02-06 16:12:55 -0800

    🎨 Actually link the next item since that's the one I want to check for.

--- py/max_item_id.py
+++ py/max_item_id.py
@@ -1,4 +1,4 @@
 import fixtures
 
 max_id = max(int(it.id) for it in fixtures.load_items())
-print(f"https://farmrpg.com/index.php#!/item.php?id={max_id}")
+print(f"https://farmrpg.com/index.php#!/item.php?id={max_id+1}")
