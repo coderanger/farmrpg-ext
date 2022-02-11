b0a4983700731507fb311f50781c68742713b9fa
Noah Kantrowitz <noah@coderanger.net>
2022-02-10 20:25:46 -0800

    Update lem_stam.py

--- py/lem_stam.py
+++ py/lem_stam.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
         stamina = loc_data["stamina"]
         drops = loc_data["drops"]["ALL"]
         data[loc] = stamina / drops
+    print("Equivalent explores per lemonade with and without the Lemon Squeezer perk:")
     for loc, stam_per_drop in sorted(data.items(), key=lambda kv: kv[1]):
         print(f"{loc}: ten={stam_per_drop * 10:.1f} twenty={stam_per_drop * 20:.1f}")
