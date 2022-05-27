6f3430f5f3c4d23db9f4fedf834008d82e5890c9
Noah Kantrowitz <noah@coderanger.net>
2022-05-27 16:45:38 -0700

    ✨ Also show missing items in the middle.

--- py/max_item_id.py
+++ py/max_item_id.py
@@ -1,4 +1,11 @@
 import fixtures
 
-max_id = max(int(it.id) for it in fixtures.load_items())
-print(f"https://farmrpg.com/index.php#!/item.php?id={max_id+1}")
+item_ids = {int(it.id) for it in fixtures.load_items()}
+max_id = max(item_ids)
+
+# Start at 11 because 1-10 were never used.
+for i in range(11, max_id + 1):
+    if i not in item_ids:
+        print(f"Missing: https://farmrpg.com/index.php#!/item.php?id={i}")
+
+print(f"New: https://farmrpg.com/index.php#!/item.php?id={max_id+1}")
